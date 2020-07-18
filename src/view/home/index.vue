e76276ef4000e9b67bbca3d0a7c4b01e84accbfb
lxz8798 <devilboy.l@qq.com>
2020-07-18 09:19:47 +0800

    更新home

--- src/view/home/index.vue
+++ src/view/home/index.vue
@@ -7,34 +7,49 @@
       <!-- 内容 -->
       <h5>为了更高效更方便更快速的开发项目而配置的基础的空白项目骨架，内置了一些常用插件，优化了一些基础配置。{{this.$config.baseUrl}}</h5>
       <p>
-        1、使用normalize.css初始化了浏览器样式，默认集成lodash、mockjs、echarts等等
+        1、使用normalize.css初始化了浏览器样式，默认集成lodash、mockjs、echarts等等。
       </p>
       <p>
-        2、项目骨架内使用vw、vh定义宽高、使用rem/px为基础单位，使用rem可以适配屏幕，px则不会。
+        2、项目骨架内使用vw、vh定义宽高、自动转换px为rem适应屏幕，可以拉伸屏幕大小看变化。
       </p>
       <p>
-        3、内嵌iView采用按需的方式
+        3、内嵌iView采用按需的方式。
       </p>
       <p>
-        4、components下的common，以public开头的组件会自动注册
+        4、components下的common，以public开头的组件会自动注册。
       </p>
       <p>
-        5、api是一个简单的api管理，直接返回一个promise，页面内使用async、await,具体可以查看/example
+        5、api是一个简单的api管理，直接返回一个promise，页面内使用async、await,具体可以查看/example。
       </p>
       <p>
-        6、vue.config.js里面默认开启了gzip和其他的一些优化配置，详情可看注释自定义配置
+        6、vue.config.js里面默认开启了gzip和其他的一些优化配置，前端开启gzip的同时后端也得支持，详情可看注释自定义配置。
       </p>
       <p>
-        7、内置了一些工具方法，可以直接this.fn使用
+        7、内置了一些工具方法，可以直接this.xxx使用。
       </p>
       <p>
-        8、默认开启iview的loadingBar，如果需要自定义的loading请自行配置
+        8、默认开启iview的loadingBar，如果需要自定义的loading请自行配置。
       </p>
       <p>
-        还有更多操作请自行发掘
+        9、全局的模态框管理。
       </p>
       <p>
-        骨架会不定期更新更多优化内容
+        10、config.externals是忽略打包，配合cdn里面使用。
+      </p>
+      <p>
+        11、使用cdn的方式分离vendors依赖，分离插件，如果需要使用import 可以在vue.config.js里面把cdn注释，同时解开main、http、router、vuex/index，store/index里面的相关import。
+      </p>
+      <p>
+        12、更新了压缩图片和打包后删除console。
+      </p>
+      <p>
+        13、新增多环境支持，和打包配置，可以使用build:pro和build:demo打包不同环境，可以使用全局config来配置变量或者使用环境变量。
+      </p>
+      <p>
+        14、增加了全局config，具体可看config/index.js。
+      </p>
+      <p>
+        15、example更新部份使用例子。
       </p>
       <p>
         ......
